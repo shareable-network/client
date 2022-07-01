abcf70c1c6703bd9b6f97aedb96063ea661a6c98
Shareable Network <shareable.network@gmail.com>
2022-07-01 10:03:33 +0200

    initial commit

---
+++ router.js
@@ -0,0 +1,20 @@
+window.addEventListener('hashchange', onRouteChanged);
+onRouteChanged();
+
+async function onRouteChanged() {
+    const routerView = document.querySelector('#router-view');
+    await loadRoute(routerView);
+    window.Alpine?.initTree(routerView);
+}
+
+async function loadRoute(routerView) {
+    switch(window.location.hash) {
+        case '#new':
+            await import('./new.js');
+            routerView.innerHTML = await fetch('./new.html').then(r => r.text());
+            break;
+        default:
+            await import('./publications.js');
+            routerView.innerHTML = await fetch('./publications.html').then(r => r.text());
+    }
+}
