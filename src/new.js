bca7d8b2cd652f5c27f6185c2cdd085edf0b0cdb
Shareable Network <shareable.network@gmail.com>
2022-08-09 16:01:31 +0200

    allow adding custom metatags

--- src/new.js
+++ src/new.js
@@ -8,14 +8,16 @@ window.newPublicationRoute = () => {
     collectionId: null,
     contentId: null,
     contentMetadata: [
-      { id: 'type', value: 'video' },
-      { id: 'src', value: null },
+      {id: 'keywords', value: '', hint: 'comma separated'},
+      {id: 'description', value: ''},
     ],
-    content: null,
+    uploading: false,
     init: async function () {
       this.publisher = await this.$store.app.getPublisher();
+      M.updateTextFields();
     },
-    save: async function () {
+    upload: async function (file) {
+      this.uploading = true;
       const authorization = 'Basic ' + btoa(
         this.$store.app.infuraIpfsProjectId + ':' +
         this.$store.app.infuraIpfsProjectSecret,
@@ -26,11 +28,18 @@ window.newPublicationRoute = () => {
         protocol: 'https',
         headers: { authorization }
       });
-      const { cid } = await ipfs.add(this.content[0]);
-      this.contentMetadata[1].value = `https://shareable.infura-ipfs.io/ipfs/${cid}`;
-
+      const { cid } = await ipfs.add(file);
+      this.contentMetadata.push({ id: 'type', value: 'video' });
+      this.contentMetadata.push({ id: 'src', value: `https://shareable.infura-ipfs.io/ipfs/${cid}` });
+      this.uploading = false;
+      this.$nextTick(() => {
+        M.updateTextFields();
+      });
+    },
+    save: async function () {
       const core = Alpine.raw(await this.$store.app.getCore());
       for (let i = 0; i < this.contentMetadata.length; i++) {
+        if(!this.contentMetadata[i].id || !this.contentMetadata[i].value) continue;
         await core.setContentMetadata(
           ethers.utils.formatBytes32String(this.channelId),
           ethers.utils.formatBytes32String(this.collectionId),
